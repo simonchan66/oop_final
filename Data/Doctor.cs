4814f5dcbd19ff2504d8dd63a929a631356b2d23
simonchan66 <formul529@gmail.com>
2023-12-01 17:45:23 -0700

    2nd version

---
+++ Data/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHybridApp2.Data
+{
+    public class Doctor
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Specialty { get; set; }
+        public string Room { get; set; }
+    }
+}
---
+++ Data/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHybridApp2.Data
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Diagnosis { get; set; }
+        public string Ward { get; set; }
+    }
+}
